acd77b7ed6914979772ee51053ac079dad0756a8
Nguyen Thanh Tai <94161651+user247-tai@users.noreply.github.com>
2024-07-07 14:53:25 +0700

    Add files via upload

---
+++ Dir/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "my_lib.h"
+int main(){
+    Fuc1();
+    printf("Hello from main\n");
+    Fuc1();
+    return 0;
+}
---
+++ Dir/src/my_lib.c
@@ -0,0 +1,4 @@
+#include "my_lib.h"
+void Fuc1(){
+    printf("Hello From Library\n");
+}
